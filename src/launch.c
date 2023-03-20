c3bc664227033423c3085717dfdb0d17fa3c9686
Francis <fcullendavison@gmail.com>
2023-03-20 17:30:51 +0100

    v0.0

--- src/launch.c
+++ src/launch.c
@@ -6,7 +6,7 @@
 /*   By: fcullen <fcullen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:42:21 by fcullen           #+#    #+#             */
-/*   Updated: 2023/03/20 17:20:06 by fcullen          ###   ########.fr       */
+/*   Updated: 2023/03/20 17:29:26 by fcullen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ void	launch_msh(void)
 {
 	char	*input;
 
-	while (input != NULL)
+	while ((input = readline("minishell: ")) != NULL)
 	{
-		input = readline("minishell: ");
+		add_history(input);
 		// parse_input(input);
 		printf("%s\n", input);
 	}
-	add_history(input);
 }
